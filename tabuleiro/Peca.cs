7e1ebae88b32810433e27d36cdc87856bbd1ef03
MarceloCRM <marcelo.crmoura@gmail.com>
2025-04-16 16:29:04 -0300

    Comecando a colocar pecas no tabuleiro

--- tabuleiro/Peca.cs
+++ tabuleiro/Peca.cs
@@ -1,14 +1,14 @@
 namespace tabuleiro {
     class Peca {
 
-        public Posicao Posicao { get; set; }
+        public Posicao? Posicao { get; set; }
         public Cor Cor { get; protected set; }
         public int qtdMovimentos { get; protected set; }
         public Tabuleiro Tabuleiro { get; protected set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        public Peca(Tabuleiro tabuleiro, Cor cor)
         {
-            this.Posicao = posicao;
+            this.Posicao = null;
             this.Cor = cor;
             this.qtdMovimentos = 0;
             this.Tabuleiro = tabuleiro;
