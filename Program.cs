971332e96e08cacadfc620a34a91c8c33e0f8856
MarceloCRM <marcelo.crmoura@gmail.com>
2025-04-24 15:41:57 -0300

    Melhorando a implementacao do metodo ImprimirTabuleiro

--- Program.cs
+++ Program.cs
@@ -2,7 +2,15 @@
 using xadrez;
 using xadrez_console;
 
-PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-
-System.Console.WriteLine(pos);
-System.Console.WriteLine(pos.ToPosicao());
+try
+{
+    Tabuleiro tab = new Tabuleiro(8, 8);
+    tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(4, 7));
+    tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(3, 4));
+    tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(2, 1));
+    Tela.ImprimirTabuleiro(tab);
+}
+catch (TabuleiroException e)
+{
+    System.Console.WriteLine(e.Message);
+}
--- Tela.cs
+++ Tela.cs
@@ -8,6 +8,7 @@ namespace xadrez_console
         {
             for (int i = 0; i < tab.Linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.Colunas; j++)
                 {
                     if (tab.Peca(i, j) == null)
@@ -16,11 +17,28 @@ namespace xadrez_console
                     }
                     else
                     {
-                        System.Console.Write(tab.Peca(i, j) + " ");
+                        ImprimirPeca(tab.Peca(i, j));
+                        System.Console.Write(" ");
                     }
                 }
                 System.Console.WriteLine();
             }
+            Console.WriteLine("  a b c d e f g h");
         }
+
+        public static void ImprimirPeca(Peca peca) 
+        {
+            if (peca.Cor == Cor.Branca)
+            {
+                System.Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
+        } 
     }
 }
