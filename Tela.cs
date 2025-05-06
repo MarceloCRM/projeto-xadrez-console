5202861f9079e922be57f0a1e0e0f47c614ed8df
MarceloCRM <marcelo.crmoura@gmail.com>
2025-05-06 15:26:20 -0300

    Tratamento de excessao no metodo LerPosicaoXadrez

--- Tela.cs
+++ Tela.cs
@@ -94,10 +94,18 @@ namespace xadrez_console
 
         public static PosicaoXadrez LerPosicaoXadrez()
         {
-            string s = Console.ReadLine();
-            char coluna = s[0];
-            int linha = int.Parse(s[1] + "");
-            return new PosicaoXadrez(coluna, linha);
+            try
+            {
+                string s = Console.ReadLine();
+                char coluna = s[0];
+                int linha = int.Parse(s[1] + "");
+                return new PosicaoXadrez(coluna, linha);
+            }
+            catch
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
+            
         }
         public static void ImprimirPeca(Peca peca) 
         {
