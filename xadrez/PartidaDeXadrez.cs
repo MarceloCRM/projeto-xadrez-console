50c4d2f6a6bee92483829b384a5ebdb94f58edfc
MarceloCRM <marcelo.crmoura@gmail.com>
2025-05-06 15:20:58 -0300

    Jogada especial promocao

--- xadrez/PartidaDeXadrez.cs
+++ xadrez/PartidaDeXadrez.cs
@@ -141,6 +141,21 @@ namespace xadrez
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
 
+            Peca p = tab.Peca(destino);
+
+            // #JogadaEspecial Promocao
+            if (p is Peao)
+            {
+                if ((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = tab.RetirarPeca(destino);
+                    Pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.Cor);
+                    tab.ColocarPeca(dama, destino);
+                    Pecas.Add(dama);
+                }
+            }
+
             if (EstaEmXeque(Adversaria(JogadorAtual)))
             {
                 Xeque = true;   
@@ -159,7 +174,6 @@ namespace xadrez
                 Turno++;
                 MudaJogador();
             }
-            Peca p = tab.Peca(destino);
 
             // #JogadaEspecial EnPassant
             if (p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
